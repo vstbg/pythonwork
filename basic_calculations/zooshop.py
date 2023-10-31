a73c4c66acb4bfa5ad260aa39d801b93b669e70b
Valentin <121805462+vstbg@users.noreply.github.com>
2023-10-31 15:09:56 +0200

    Create zooshop.py

---
+++ basic_calculations/zooshop.py
@@ -0,0 +1,11 @@
+# Read user input
+dogs_food = int(input())
+cats_food = int(input())
+
+# Logic
+dogs_total = dogs_food * 2.5
+cats_total = cats_food * 4
+total = dogs_total + cats_total
+
+# Print Output
+print(f'{total} lv.')

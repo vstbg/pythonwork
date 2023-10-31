c0922bf483280e25f4a68bba7e5dc3d83fb1bcc9
Valentin <121805462+vstbg@users.noreply.github.com>
2023-10-31 15:13:32 +0200

    Create architect_projects.py

---
+++ basic_calculations/architect_projects.py
@@ -0,0 +1,9 @@
+# Read user input
+name = input()
+projects = int(input())
+
+# Logic
+projects_time = projects * 3
+
+# Output
+print(f'The architect {name} will need {projects_time} hours to complete {projects} project/s.')

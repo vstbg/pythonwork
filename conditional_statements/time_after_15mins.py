b06fef82fb1491a773e4e85cfe0873834810e62f
Valentin <121805462+vstbg@users.noreply.github.com>
2023-11-02 17:30:28 +0200

    Create time_after_15mins.py

---
+++ conditional_statements/time_after_15mins.py
@@ -0,0 +1,14 @@
+hours = int(input())
+minutes = int(input()) + 15
+
+if minutes > 59:
+    hours += 1
+    minutes -= 60
+
+if hours > 23:
+    hours -= 24
+
+if minutes < 10:
+    print(f"{hours}:0{minutes}")
+else:
+    print(f"{hours}:{minutes}")

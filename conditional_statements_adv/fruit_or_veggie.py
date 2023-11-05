54fec34bf5c515ec24835a2de75d3fd04a287f2c
Valentin <121805462+vstbg@users.noreply.github.com>
2023-11-05 16:07:32 +0200

    Create fruit_or_veggie.py

---
+++ conditional_statements_adv/fruit_or_veggie.py
@@ -0,0 +1,18 @@
+# Read user input
+product = input()
+
+# Logics
+if product == "banana" \
+        or product == "apple" \
+        or product == "kiwi" \
+        or product == "cherry" \
+        or product == "lemon" \
+        or product == "grapes":
+    print("fruit")
+elif product == "tomato" \
+        or product == "cucumber" \
+        or product == "pepper" \
+        or product == "carrot":
+    print("vegetable")
+else:
+    print("unknown")

7982fa637297dbb1f1725d3b8b9ee9845952ff1a
Valentin <121805462+vstbg@users.noreply.github.com>
2023-11-05 16:06:51 +0200

    Create day_week.py

---
+++ conditional_statements_adv/day_week.py
@@ -0,0 +1,21 @@
+# Input
+day = int(input())
+
+# Logics + print output
+
+if day == 1:
+    print('Monday')
+elif day == 2:
+    print('Tuesday')
+elif day == 3:
+    print('Wednesday')
+elif day == 4:
+    print('Thursday')
+elif day == 5:
+    print('Friday')
+elif day == 6:
+    print('Saturday')
+elif day == 7:
+    print('Sunday')
+else :
+    print('Error')

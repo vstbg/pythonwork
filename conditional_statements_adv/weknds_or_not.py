56a6a68174e54a3fe314941e074e8b7ed22517d9
Valentin <121805462+vstbg@users.noreply.github.com>
2023-11-05 16:26:23 +0200

    Create weknds_or_not.py

---
+++ conditional_statements_adv/weknds_or_not.py
@@ -0,0 +1,21 @@
+# Read input
+day = input()
+
+# Logics
+
+if day == "Monday":
+    print("Working day")
+elif day == "Tuesday":
+    print("Working day")
+elif day == "Wednesday":
+    print("Working day")
+elif day == "Thursday":
+    print("Working day")
+elif day == "Friday":
+    print("Working day")
+elif day == "Saturday":
+    print("Weekend")
+elif day == "Sunday":
+    print("Weekend")
+else:
+    print("Error")

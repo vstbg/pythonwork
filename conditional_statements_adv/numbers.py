473e2e73ec01a0fa9f6b4042fd624f44a0eaec64
Valentin <121805462+vstbg@users.noreply.github.com>
2023-11-05 17:13:36 +0200

    Create numbers.py

---
+++ conditional_statements_adv/numbers.py
@@ -0,0 +1,23 @@
+# Input
+num1 = int(input())
+num2 = int(input())
+operator = input()
+
+result = None
+
+# Logics
+if operator == "+":
+    result = f"{num1} + {num2} = {num1 + num2}" + (" - even" if (num1 + num2) % 2 == 0 else " - odd")
+elif operator == "-":
+    result = f"{num1} - {num2} = {num1 - num2}" + (" - even" if (num1 - num2) % 2 == 0 else " - odd")
+elif operator == "*":
+    result = f"{num1} * {num2} = {num1 * num2}" + (" - even" if (num1 * num2) % 2 == 0 else " - odd")
+elif num2 == 0:
+    result = f"Cannot divide {num1} by zero"
+elif operator == "/":
+    result = f"{num1} / {num2} = {num1 / num2:.2f}"
+elif operator == "%":
+    result = f"{num1} % {num2} = {num1 % num2}"
+
+# Print output
+print(result)

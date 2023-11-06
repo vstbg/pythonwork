17c66dd7f23dbd650cc4860fc169f20b828d6aed
Valentin <121805462+vstbg@users.noreply.github.com>
2023-11-06 22:28:28 +0200

    Create salary.py

---
+++ for_loop/salary.py
@@ -0,0 +1,20 @@
+# Input
+n = int(input())
+salary = int(input())
+
+# Logics
+for _ in range(n):
+    tab = input()
+
+    if tab == "Facebook":
+        salary -= 150
+    elif tab == "Instagram":
+        salary -= 100
+    elif tab == "Reddit":
+        salary -= 50
+
+    if salary <= 0:
+        print(f"You have lost your salary.")
+        break
+else:
+    print(salary)

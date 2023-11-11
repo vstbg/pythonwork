4444a61893a7aae72d2f4c5601f0331e961430b0
Valentin <121805462+vstbg@users.noreply.github.com>
2023-11-11 15:15:01 +0200

    Create moving.py

---
+++ while_loop/moving.py
@@ -0,0 +1,15 @@
+# Input
+total_space = int(input()) * int(input()) * int(input())
+
+# Logics
+space_filled = 0
+
+while space_filled < total_space:
+    box = input()
+
+    if box == "Done":
+        print(f"{total_space - space_filled} Cubic meters left.")
+        break
+    space_filled += int(box)
+else:
+    print(f"No more free space! You need {space_filled - total_space} Cubic meters more.")
